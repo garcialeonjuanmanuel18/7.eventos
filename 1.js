ab84bd1de7cccd330c0fb464efbea579db1eec96
Juan Manuel García León <garcialeonjuanmanuel@gmail.com>
2026-02-06 15:58:19 +0100

    sigue sin ir

--- 1.js
+++ 1.js
@@ -4,5 +4,10 @@ const p = document.getElementById("p");
 h1.addEventListener("click", ocultar);
 
 function ocultar(event){
-   if();
+   if(p.style.display === none){
+      p.style.display = "block";
+   } else {
+      p.style.display = "none";
+   }
+   
 }
